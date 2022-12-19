65b543630ff9228086b064b0250dbdf588ce6c44
marxtinn <butarbutar.m@gmail.com>
2022-12-19 15:30:23 +0700

    Excercise

--- day-02excercise.js
+++ day-02excercise.js
@@ -1,3 +1,5 @@
+// day-02excercise.js
+
 // 1. Convert Celcius to Fahrenheit. (Formula: °F = (9/5 x °C) + 32)
 // let cel = 90;
 // const fah =  9 / 5 * cel + 32;
@@ -26,7 +28,7 @@
 // }
 // console.log(bool ? `${bil} is a prime number` : `${bil} is not a prime number`)
 
-// 4. Find the sum of the numbers 1 to N, e.g.: 3 -> 1+2+3=6.  
+// 4. Find the sum of the numbers 1 to N, e.g.: 3 -> 1+2+3=6.
 // let num = 5;
 // let sum = 0;
 // let message = " ";
@@ -50,10 +52,12 @@
 
 // 6. Print the first N fibonacci numbers
 const num = 8;
-let a= 0, b = 1, next;
-for (i = 1 ; i <= num ; i++) {
-    console.log(a);
-    next = a + b;
-    a=b;
-    b=next;
+let a = 0,
+  b = 1,
+  next;
+for (i = 1; i <= num; i++) {
+  console.log(a);
+  next = a + b;
+  a = b;
+  b = next;
 }
